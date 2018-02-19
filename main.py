6a4482eb227db780f12e85a3c0f31ff74f476e30
kevin <googlevsbing@126.com>
2018-02-19 23:07:41 +0900

    flask added

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ def start():
 
     return render_template('index.html')
 
-
-if __name__ == '__main__':
+# if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='0.0.0.0', port=80, debug=True)
+    # app.run(host='0.0.0.0', port=80, debug=True)
 #	app.run()
